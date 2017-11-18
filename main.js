2cae0b5c47f2926994bf80f902cfa591f72c8e64
Petru Conduraru <petruconduraru@outlook.com>
2017-11-18 13:11:32 +0200

    fixed changing squares to picked colors

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
 var colors = [
-  "rbg(255, 0, 0)",
-  "rbg(255, 255, 0)",
-  "rbg(0, 0, 255)",
-  "rbg(0, 255, 255)",
-  "rbg(0, 0, 255)",
-  "rbg(255, 0, 255)"
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(255, 255, 255)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
 ];
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector('#message');
 
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
+  // Add initial colors to squares
   squares[i].style.backgroundColor = colors[i];
+  // Add click listeners to squares
+  squares[i].addEventListener('click', function(){
+    //grab color of clicked square
+    var clickedColor = this.style.backgroundColor; 
+    //compare color to pickedColor
+    if(clickedColor === pickedColor) {
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
+    } else {
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
+    }
+  });
 }
+
+function changeColors(color){
+  //loop trough all squares
+  for(var i = 0; i < colors.length; i++){
+    //change each color to match given color
+    squares[i].style.backgroundColor = color;
+  }
+  
+}
+
